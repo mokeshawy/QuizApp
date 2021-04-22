16788860899c15335603b79bd73e4055a4c6976c
mohamed <moahmed_gsm@msn.com>
2021-04-23 01:41:57 +0200

    operation fragment whit view modle

--- app/src/main/java/com/example/quizapp/mainfragment/MainFragment.kt
+++ app/src/main/java/com/example/quizapp/mainfragment/MainFragment.kt
@@ -7,21 +7,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
+    lateinit var binding                : FragmentMainBinding
+    private val mainFragmentViewModel   : MainFragmentViewModel by viewModels()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        binding.lifecycleOwner          = this
+        binding.mainFragmentVarModel    = mainFragmentViewModel
 
     }
 }
--- app/src/main/java/com/example/quizapp/quizquestionsfragment/QuizQuestionsFragment.kt
+++ app/src/main/java/com/example/quizapp/quizquestionsfragment/QuizQuestionsFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentQuizQuestionsBinding
 
 class QuizQuestionsFragment : Fragment() {
 
+    lateinit var binding : FragmentQuizQuestionsBinding
+    private val quizQuestionsViewModel : QuizQuestionsViewModel by viewModels()
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz_questions, container, false)
+        binding = FragmentQuizQuestionsBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.lifecycleOwner          = this
+        binding.quizQuestionsVarModel   = quizQuestionsViewModel
     }
 }
